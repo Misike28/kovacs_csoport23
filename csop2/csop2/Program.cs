1de8c315cd56d594d30657977aa39f98c69988b9
Kovács Mihály <117653038+Misike28@users.noreply.github.com>
2024-11-04 10:10:45 +0100

    Add files via upload

---
+++ csop2/csop2/Program.cs
@@ -0,0 +1,40 @@
+﻿
+using csop2;
+using System.Text;
+
+List<Versenyzok> versenyzok = [];
+
+using StreamReader sr = new("..\\..\\..\\src\\forras.txt", Encoding.UTF8);
+while (!sr.EndOfStream) versenyzok.Add(new(sr.ReadLine()));
+
+var f1 = versenyzok.Count(v => v.kategoria == "elit junior");
+Console.WriteLine($"elit junior versenyzok: {f1}");
+var f2 = versenyzok.Where(v => v.nem).Average(v => DateTime.Now.Year - v.szul);
+Console.WriteLine($"Női életkor átlag {f2:0.00}");
+var f3 = versenyzok.Sum(v => v.versenyidok["úszás"].TotalHours);
+Console.WriteLine($"Össz úszás időtartam: {f3:0:00}");
+var f4 = versenyzok.Average(v => v.versenyidok["úszás"].TotalHours);
+Console.WriteLine($"Versenyzők átlag úszása: {f4:0.00}");
+var f5 = versenyzok.Where(v => !v.nem).MinBy(v => v.versenyidok.Sum(v => v.Value.TotalSeconds));
+Console.WriteLine($"női győztes: {f5}");
+var f6 = versenyzok.GroupBy(v => v.nem).OrderBy(v => v.Key);
+foreach (var f in f6)
+{
+    Console.WriteLine($"{(f.Key ? "Férfi" : "Nő")}: {f.Count()}");
+}
+//--------------------------------
+var f7 = versenyzok.Count(v => v.kategoria == "25-29");
+Console.WriteLine($"25-29 kategóriában lévő versenyzők száma {f7}");
+var f8 = versenyzok.Average(v => DateTime.Now.Year - v.szul );
+Console.WriteLine($"Versenyzők átlag életkora: {f8:0.00}");
+var f9 = versenyzok.Sum(v => v.versenyidok["úszás"].TotalHours);
+Console.WriteLine($"Össz úszás időtartam: {f3:0:00}");
+var f10 = versenyzok.Where(v=>v.kategoria=="elit junior").Average(v => v.versenyidok["úszás"].TotalHours);
+Console.WriteLine($"Versenyzők átlag úszása: {f4:0.00}");
+var f11 = versenyzok.Where(v => v.nem).MinBy(v => v.versenyidok.Sum(v => v.Value.TotalSeconds));
+Console.WriteLine($"Férfi győztes: {f5}");
+var f12 = versenyzok.GroupBy(v => v.nem).OrderBy(v => v.Key);
+foreach (var f in f12)
+{
+    Console.WriteLine($"{(f.Key ? "Férfi" : "Nő")}: {f.Count()}");
+}
---
+++ csop2/csop2/Versenyzok.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csop2
+{
+    internal class Versenyzok
+    {
+        public string nev { get; set; }
+        public int szul { get; set; }
+        public string rajtszam { get; set; }
+        public bool nem { get; set; }
+        public string kategoria { get; set; }
+        public Dictionary<string, TimeSpan> versenyidok { get; set; }
+        public override string ToString() =>
+        $"[{rajtszam} {nev} ({(nem ? "Férfi" : "Nő")}, {kategoria},)]";
+
+
+        public Versenyzok(string sor)
+        {
+            string[] v = sor.Split(';');
+            nev = v[0];
+            szul = int.Parse(v[1]);
+            rajtszam = v[2];
+            nem = v[3] == "f";
+            kategoria = v[4];
+
+            versenyidok = new()
+        {
+            {"úszás",TimeSpan.Parse(v[5])},
+            {"I. Depó",TimeSpan.Parse(v[6])},
+            {"kerékpár",TimeSpan.Parse(v[7])},
+            {"II. Depó",TimeSpan.Parse(v[8])},
+            {"futás",TimeSpan.Parse(v[9])}
+        };
+        }
+    }
+}
